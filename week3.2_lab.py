3cbf29df92292b777fa2fc2ab4df4e1a1cc19b61
Jack Armstrong <jackarmstrong819@gmail.com>
2025-10-28 13:42:52 +0000

    lol

---
+++ week3.2_lab.py
@@ -0,0 +1,8 @@
+
+costs = [15.00, 12.50, 3.75, 40.25, 6.99]
+total_cost = 0
+
+for cost in costs:
+    total_cost += cost
+
+print(f"£{total_cost}")
---
+++ week3_lab.py
@@ -0,0 +1,5 @@
+ 
+num = 0
+for i in range(11):
+    num2 = i + num
+    print(num2)
