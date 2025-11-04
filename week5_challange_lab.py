86494d7eb21254f2cfcb080651b79bad82abd27e
Jack-Armstrong <jackarmstrong819@gmail.com>
2025-11-04 14:52:16 +0000

    F

--- week5_challange_lab.py
+++ week5_challange_lab.py
@@ -1,5 +1,5 @@
 def calculate_factorial(n):
-    total = 0 breakpoint # <-- Bug #1
+    total = 0  # <-- Bug #1
     for i in range(1, n + 1):
         total = total * i # <-- Bug #2 (logic is flawed)
     return total
