3f6565a72338a39d3cfe40b1d4ba1a666fbd3521
Jack-Armstrong <jackarmstrong819@gmail.com>
2025-11-11 14:55:53 +0000

    f

---
+++ week6.4_lab.py
@@ -0,0 +1,10 @@
+questions = ["What is 2+2?", "What is the capital of France?", "Whatkeyword defines a function?"]
+answers = ["4", "Paris", "def"]
+score = 0
+
+for i in range(len(questions)):
+    user_answer = input(questions[i])
+    if user_answer == answers[i]:
+        print("correct")
+        score += 1
+print(f"{score}/3")
